4c09e500033a926ac169c804f5f136082083c693
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-02-18 22:44:28 +0900

    [ feat ] @EnableJpaAuditing 활성화

--- src/main/java/com/elasticsearch/practice/product/ElasticsearchPracticeApplication.java
+++ src/main/java/com/elasticsearch/practice/product/ElasticsearchPracticeApplication.java
@@ -2,8 +2,10 @@ package com.elasticsearch.practice.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ElasticsearchPracticeApplication {
 
     public static void main(String[] args) {
