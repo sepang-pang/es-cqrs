449088539b065311455accbb313096200c94578e
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-02-18 20:22:08 +0900

    init commit

---
+++ src/main/java/com/elasticsearch/practice/prodcut/ElasticsearchPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.practice.prodcut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elasticsearch/practice/prodcut/ElasticsearchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.practice.prodcut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
