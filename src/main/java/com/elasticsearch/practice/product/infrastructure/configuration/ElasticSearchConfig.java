f6985c6b3a14e6743d3f8ca0f49859de6db0056f
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-02-19 08:13:55 +0900

    [ feat ] ElasticSearchConfig 추가

---
+++ src/main/java/com/elasticsearch/practice/product/infrastructure/configuration/ElasticSearchConfig.java
@@ -0,0 +1,22 @@
+package com.elasticsearch.practice.product.infrastructure.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.elasticsearch.host}")
+    private String host;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(host)
+                .build();
+    }
+}
